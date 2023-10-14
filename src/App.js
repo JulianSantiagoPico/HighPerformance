45c1bb221b29868a96cc69565cb7b7ed775d7028
HellBlaze <julpic08@gmail.com>
2023-10-13 23:20:20 -0500

    Se añade el navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import NavBar from './Components/NavBar/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+
+        <NavBar />
+
+          <Routes>
+
+            <Route path='/'/>
+
+          </Routes>
+
+        <Footer />
+
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <div>
+      <Navbar expand="lg" bg='white'>
+        <Container>
+          <Navbar.Brand href="#home"><img src="https://res.cloudinary.com/duqtm5tnb/image/upload/v1697254856/HighPerformance/s8oasljxb7zori2vxkqc.png" alt="Logo" style={{width:"20vw"}}/></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto" style={{marginLeft:"7.5rem", fontSize:"1.15rem", display:"flex", gap:"20px"}}>
+              <Link to="/" style={{textDecoration:"none", color:"black", paddingTop:"0.5rem"}}> HOME </Link>
+              <Link to="/" style={{textDecoration:"none", color:"black", paddingTop:"0.5rem"}}> ABOUT US </Link>
+              <Link to="/" style={{textDecoration:"none", color:"black", paddingTop:"0.5rem"}}> SERVICES </Link>
+              <Link to="/" style={{textDecoration:"none", color:"black", paddingTop:"0.5rem"}}> INSURANCE </Link>
+              <Link to="/" style={{textDecoration:"none", color:"black", paddingTop:"0.5rem"}}> CONTACT </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div style={{backgroundColor:"red"}}>
 
-        
-
+      </div>
     </div>
   )
 }
