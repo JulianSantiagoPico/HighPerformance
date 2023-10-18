a90e33ff9a17624e4103c747d8e48eaf8cf2eb40
HellBlaze <julpic08@gmail.com>
2023-10-17 22:11:35 -0500

    Primeros cambios al formulario de contacto

--- src/Components/ContactContainer/ContactContainer.jsx
+++ src/Components/ContactContainer/ContactContainer.jsx
@@ -1,15 +1,13 @@
 import Card from 'react-bootstrap/Card';
+import ContactForm from '../ContactForm/ContactForm';
 
 const ContactContainer = () => {
   return (
     <div>
-        <div style={{display:"flex", justifyContent:"center", marginTop:"1rem"}}>
-        <Card style={{width:"98%", display:"flex", alignItems:"center", padding:"1rem"}}>
-          <Card.Img variant="bottom" src="" />
+        <div style={{display:"flex", justifyContent:"center", marginTop:"1rem", marginBottom:"1rem"}}>
+        <Card style={{width:"98%", display:"flex", alignItems:"center", padding:"1rem", backgroundColor:"#fafafa"}}>
           <Card.Body>
-            <Card.Text>
-              We're in the contact page.
-            </Card.Text>
+              <ContactForm />
           </Card.Body>
         </Card>
       </div>
---
+++ src/Components/ContactForm/ContactForm.jsx
@@ -0,0 +1,27 @@
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+const ContactForm = () => {
+  return (
+    <div>
+        <h1 className='mb-3' style={{fontSize:"3rem", textAlign:"center"}}>Have questions or ready to schedule repairs? <br/> Contact us now!</h1>
+        <div>
+        <Form>
+
+            <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
+                <Form.Control type="email" placeholder="Email Adress" />
+            </FloatingLabel>
+
+            <FloatingLabel controlId="floatingInput" label="Password" className='mb-3'>
+                <Form.Control type="password" placeholder="Password" />
+            </FloatingLabel>
+
+            <Button type="submit">Submit</Button>
+        </Form>
+        </div>
+    </div>
+  )
+}
+
+export default ContactForm
