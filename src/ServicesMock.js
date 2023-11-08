0b5ddef9a2202db3fe8e72ccc31385feddcf0e8a
HellBlaze <julpic08@gmail.com>
2023-11-07 21:35:41 -0500

    Se añade nueva logica de productos (pendiente)

---
+++ src/ServicesMock.js
@@ -0,0 +1,51 @@
+export const services = [
+  {
+    id: 1,
+    title: "Blind Spot Detection",
+    description:
+      "Blind spot car detection systems watch where you can't. The driver assistance technology tracks vehicles approaching alongside or behind you. If you're indicating to change lanes, the system will warn you in a couple of different ways depending on the make of your car.",
+    img: "https://res.cloudinary.com/duqtm5tnb/image/upload/v1699409830/HighPerformance/Services/blind-spot-detection_stage_v2_1_1_isvuxz.jpg",
+  },
+  {
+    id: 2,
+    title: "Automatic Emergency Braking",
+    description:
+      "Automatic emergency braking (AEB) is a safety system that can identify when a possible collision is about to occur and responds by autonomously activating the brakes to slow a vehicle prior to impact or bring it to a stop to avoid a collision.",
+    img: "https://res.cloudinary.com/duqtm5tnb/image/upload/v1699409665/HighPerformance/Services/f2bb1d32605139607e70e1e3ed6add70d9c7a5e6_1_qpkwuh.jpg",
+  },
+  {
+    id: 3,
+    title: "Pedestrian Detection",
+    description:
+      "Pedestrian detection is an advanced driver assistance system (ADAS). It uses sensors, cameras, and artificial intelligence to identify pedestrians near vehicles. By analyzing the surrounding environment, the system can recognize human forms and movements, helping to prevent or mitigate potential collisions between vehicles and pedestrians.",
+    img: "https://res.cloudinary.com/duqtm5tnb/image/upload/v1699409665/HighPerformance/Services/Volvo-Pedestrian-and-Cyclist-Detection-System-2-625x351_1_m4adnd.jpg",
+  },
+  {
+    id: 4,
+    title: "Adaptive Cruise Control",
+    description:
+      "Adaptive cruise control (ACC) is a system designed to help vehicles maintain a safe following distance and stay within the speed limit. This system adjusts a car's speed automatically so drivers don't have to.",
+    img: "https://res.cloudinary.com/duqtm5tnb/image/upload/v1699409666/HighPerformance/Services/control-buttons-on-steering-wheel-in-a-modern-car-royalty-free-image-1591720736_1_bb8mqy.jpg",
+  },
+  {
+    id: 5,
+    title: "Traffic Sign Recognition",
+    description:
+      "Traffic Sign Recognition (TSR) is an advanced driver assistance system (ADAS) that recognizes and relays traffic sign information to drivers via the instrument panel, multimedia, or heads-up display. In the United States, TSR systems can recognize speed limit signs, do not enter signs, and stop signs.",
+    img: "https://res.cloudinary.com/duqtm5tnb/image/upload/v1699409666/HighPerformance/Services/traffic-sign-recognition-scaled-pe6vqli01yeabbmk18mrv77sitcqrucldm5capdoco_1_npg8se.jpg",
+  },
+  {
+    id: 6,
+    title: "Lane Departure Warning",
+    description:
+      "Lane departure warning is designed to help you avoid crashes due to drifting or departing your lane. The system detects lane markers and alerts you when a tire touches a lane marker. The warning is usually a flashing indicator and/or it beeps from the corresponding side.",
+    img: "https://res.cloudinary.com/duqtm5tnb/image/upload/v1699409665/HighPerformance/Services/csm_lane_20departure_20warning_e03c28b9ca_ur2irp.jpg",
+  },
+  {
+    id: 7,
+    title: "Lane Keeping Assist",
+    description:
+      "Lane keeping assistance (LKA) gives steering support to assist the driver in preventing the vehicle from departing the lane.",
+    img: "https://res.cloudinary.com/duqtm5tnb/image/upload/v1699409830/HighPerformance/Services/blind-spot-detection_stage_v2_1_1_isvuxz.jpg",
+  },
+];
